ff40ac76cca669dacf52a2852098c888cfc50de0
nteste01 <31543916+Tebzaaa@users.noreply.github.com>
2017-11-09 23:08:58 -0600

    Update Company.java

--- src/exercise5/Company.java
+++ src/exercise5/Company.java
@@ -3,8 +3,32 @@ package exercise5;
 /**
  * Class Company
  * Task 4: Create a class Company with the following private data members: String name, double stockPrice. Implement a setter method for the stockPrice.
- * @author yasiro01
- */
+ * @author Teboho Samuel Nteso
+ * Assihnement: Excercise 5
+ * */
 public class Company {
-  
+    private String name;
+    private Double stockPrice;
+    
+    //Constructor
+    public Company(String name, Double stockPrice){
+        this.name = name;
+        this.stockPrice = stockPrice;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    public Double getStockPrice(){
+        return this.stockPrice;
+    }
+    public Double setStockPrice() {
+        return this.stockPrice = stockPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" + "name=" + name + ", stockPrice=" + stockPrice + '}';
+    }
+    
 }
